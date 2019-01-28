f5e3c8c0f4f5bba6377952a03e46873eceab28ba
Will Barker <willbarkerwork@gmail.com>
2019-01-28 13:52:55 +0000

    render message when no ingredients added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = ({ ingredients }) => {
-  const transformedIngredients = Object.keys(ingredients).map(ingKey => {
-    return [...Array(ingredients[ingKey])].map((_, i) => (
-      <BurgerIngredient key={ingKey + i} type={ingKey} />
-    ));
-  });
+  let transformedIngredients = Object.keys(ingredients)
+    .map(ingKey => {
+      return [...Array(ingredients[ingKey])].map((_, i) => (
+        <BurgerIngredient key={ingKey + i} type={ingKey} />
+      ));
+    })
+    .reduce((acc, el) => {
+      return acc.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
