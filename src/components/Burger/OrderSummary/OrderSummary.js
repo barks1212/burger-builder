d05e34a520374737c5d9695e334fa43215b60bfb
Will Barker <willbarkerwork@gmail.com>
2019-02-21 13:44:40 +0000

    render custom button components with click handlers to cancel and continue

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
-const orderSummary = ({ ingredients }) => {
+const orderSummary = ({
+  ingredients,
+  purchaseCancelled,
+  purchaseContinued,
+  price
+}) => {
   const ingredientSummary = Object.keys(ingredients).map(ingKey => {
     return (
       <li key={ingKey}>
@@ -16,9 +22,19 @@ const orderSummary = ({ ingredients }) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>
+          Total Price:
+          {price.toFixed(2)}
+        </strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button btnType="Danger" clicked={purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
