b65d043a08002c3af924e91548b8a157dc06b3c3
Will Barker <willbarkerwork@gmail.com>
2019-04-25 11:38:32 +0100

    move axios request to checkout, redirect to home after submit and hook up databse to form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,6 +98,7 @@ class BurgerBuilder extends React.Component {
           "=" +
           encodeURIComponent(this.state.ingredients[i])
       );
+      queryParams.push("price=" + this.state.totalPrice);
     }
 
     const queryString = queryParams.join("&");
@@ -106,30 +107,6 @@ class BurgerBuilder extends React.Component {
       pathname: "/checkout",
       search: "?" + queryString
     });
-    // alert("You continue");
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Will Barker",
-    //     address: {
-    //       street: "Test street 1",
-    //       zipCode: "12345",
-    //       country: "UK"
-    //     },
-    //     email: "test@test.com"
-    //   },
-    //   deliveryMethod: "fastest"
-    // };
-    // axios
-    //   .post("/orders.json", order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
   };
 
   render() {
