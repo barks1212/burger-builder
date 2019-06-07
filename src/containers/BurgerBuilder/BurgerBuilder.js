a109b617b43ab0d5e535551ca76ef6dec9fec7a3
Will Barker <willbarkerwork@gmail.com>
2019-06-07 15:04:14 +0100

    set up reducer to add and remove ingreidnets

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,14 +29,14 @@ class BurgerBuilder extends React.Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://react-my-burger-8606c.firebaseio.com/ingredients.json")
-      .then(response => {
-        this.setState({ ingredients: response.data });
-      })
-      .catch(error => {
-        this.setState({ error: true });
-      });
+    //   axios
+    //     .get("https://react-my-burger-8606c.firebaseio.com/ingredients.json")
+    //     .then(response => {
+    //       this.setState({ ingredients: response.data });
+    //     })
+    //     .catch(error => {
+    //       this.setState({ error: true });
+    //     });
   }
 
   updatePurchaseState = ingredients => {
