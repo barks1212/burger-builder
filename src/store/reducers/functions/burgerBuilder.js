9844a517cec4a0295009709f3140bd0a43a52bde
Will Barker <willbarkerwork@gmail.com>
2019-07-08 20:19:33 +0100

    extract reducer functions into utils folder

---
+++ src/store/reducers/functions/burgerBuilder.js
@@ -0,0 +1,50 @@
+import { updateObject } from "../../utility";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
+export const addIngredient = (state, action) => {
+  const updatedIngredient = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+  };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  };
+  return updateObject(state, updatedState);
+};
+
+export const removeIngredient = (state, action) => {
+  const updatedIng = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+  };
+  const updatedIngs = updateObject(state.ingredients, updatedIng);
+  const updatedSt = {
+    ingredients: updatedIngs,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  };
+  return updateObject(state, updatedSt);
+};
+
+export const setIngredients = (state, action) => {
+  const { salad, bacon, cheese, meat } = action.ingredients;
+  return updateObject(state, {
+    ingredients: {
+      salad,
+      bacon,
+      cheese,
+      meat
+    },
+    totalPrice: 4,
+    error: false
+  });
+};
+
+export const fetchIngredientsFailed = (state, action) => {
+  return updateObject(state, { error: true });
+};
---
+++ src/store/reducers/functions/order.js
@@ -0,0 +1,34 @@
+import { updateObject } from "../../utility";
+
+export const purchaseInit = (state, action) => {
+  return updateObject(state, { purchased: false });
+};
+
+export const purchaseBurgerStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+
+export const purchaseBurgerSuccess = (state, action) => {
+  const newOrder = updateObject(action.orderData, { id: action.orderId });
+  return updateObject(state, {
+    loading: false,
+    purchased: true,
+    orders: state.orders.concat(newOrder)
+  });
+};
+
+export const purchaseBurgerFail = (state, action) => {
+  return updateObject(state, { loading: false });
+};
+
+export const fetchOrdersStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+
+export const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, { orders: action.orders, loading: false });
+};
+
+export const fetchOrdersFail = (state, action) => {
+  return updateObject(state, { loading: false });
+};
