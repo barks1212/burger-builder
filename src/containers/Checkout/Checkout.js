fcb9027fe062feb977c98bcfa4203aca125290ec
Will Barker <willbarkerwork@gmail.com>
2019-07-08 19:24:00 +0100

    conditional redirect upon purchase

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
 
+import * as actions from "../../store/actions/index";
+
 class Checkout extends React.Component {
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
@@ -17,9 +19,12 @@ class Checkout extends React.Component {
   render() {
     let summary = <Redirect to="/" />;
     if (this.props.ings) {
-      console.log("hi");
+      const purchasedRedirect = this.props.purchased ? (
+        <Redirect to="/" />
+      ) : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
@@ -38,7 +43,8 @@ class Checkout extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 
