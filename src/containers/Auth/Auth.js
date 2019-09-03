cc1808c0f15edb530ec00ec9c6f1729083f2ee61
barks1212 <willbarkerwork@gmail.com>
2019-09-03 10:56:33 +0100

    redirect to home on login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
 import classes from './Auth.module.css';
 
@@ -123,8 +124,14 @@ class Auth extends React.Component {
                 );
             };
 
+            let authRedirect = null;
+            if (this.props.isAuthenticated) {
+                authRedirect = <Redirect to="/" />
+            };
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -141,7 +148,8 @@ class Auth extends React.Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
