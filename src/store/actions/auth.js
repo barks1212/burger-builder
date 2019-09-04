207549eb4557c0ad5a24b0b27d47663c5560b297
barks1212 <willbarkerwork@gmail.com>
2019-09-04 14:00:32 +0100

    remove console logs

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -56,7 +56,6 @@ export const auth = (email, password, isSignUp) => {
 
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
@@ -65,7 +64,6 @@ export const auth = (email, password, isSignUp) => {
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                console.log(err);
                 dispatch(authFail(err.response.data.error));
             })
     }
@@ -85,7 +83,6 @@ export const authCheckState = () => {
             dispatch(logout());
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            console.log(expirationDate, new Date())
             if (expirationDate <= new Date()) {
                 dispatch(logout());
             } else {
